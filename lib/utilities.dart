82a3c6057c0c85a0eb5dcb6437164558f226f2c8
salah taha <salahtaha065@gmail.com>
2020-03-05 00:06:54 +0200

    cleanning more

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 final _fireStore = Firestore.instance;
 final usersRef = _fireStore.collection('users');
-final storageRef = FirebaseStorage.instance.ref();
 final businessRef = _fireStore.collection('businessCards');
 final chatRoomRef = _fireStore.collection('chatRoom');
 final questionRef = _fireStore.collection('questions');
